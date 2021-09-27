736c3be4d881dd08c9f52444ca4a0b83a00d6b6b
Sunil Kumar <ssunil.uk@gmail.com>
2021-09-27 08:41:15 +0530

    cart css added

--- src/Component/CartList.jsx
+++ src/Component/CartList.jsx
@@ -1,8 +1,8 @@
 import React, {useState} from 'react'
 import LocalOfferOutlinedIcon from '@mui/icons-material/LocalOfferOutlined';
 import LoyaltyOutlinedIcon from '@mui/icons-material/LoyaltyOutlined';
-import {Redirect} from "react-router-dom"
-
+import styles from "./CartList.module.css"
+import ArrowDropDownOutlinedIcon from '@mui/icons-material/ArrowDropDownOutlined';
 
 export const CartList = ({detail, img, handleDelete}) => {
     const [mrp, setMrp] = useState(0);
@@ -13,88 +13,97 @@ export const CartList = ({detail, img, handleDelete}) => {
     console.log("cartList = ",detail.productItem, detail.id)
 
     return (
-        <div>
-            <div>
-                <div>
+        <div className={styles.content}>
+            <div className={styles.detail}>
+                <div className={styles.pincheck}>
                     <div>Check delivery time & services</div>
                     <button>ENTER PIN CODE</button>
                 </div>
-                <div>
-                    <div>
+                <div className={styles.offers}>
+                    <main>
                         <LocalOfferOutlinedIcon />
                         <div>Available Offers</div>
-                    </div>
+                    </main>
                     <div>10% Instant Discount with Standard Chartered Credit and Debit Card on a min spend of Rs. 3000 TCA</div>
-                    <div>Show More</div>
+                    <div className={styles.fontPink}>Show More</div>
                 </div>
-                <div>
-                    <div>ITEMS</div>
-                    <div>
+                <div className={styles.items}>
+                    <main>ITEMS SELECTED</main>
+                    <div >
                         <button onClick={() => handleDelete(detail.id)}>REMOVE</button>
                         <div>|</div>
                         <button>MOVE TO WISHLIST</button>
                     </div>
                 </div>
-                <div>
+                <div className={styles.description}>
                     <div>
-                        <img src={img} alt="photo" />
+                        <img src={img} alt="productphoto" />
                     </div>
-                    <div>
+                    <div className={styles.info}>
                         <main>{detail.productItem.name}</main>
                         <div>{detail.productItem.description}</div>
                         <div>{detail.productItem.seller}</div>
-                        <div>
-                            <div>
+                        <div className={styles.size}>
+                            <main>
                                 <div>Size:</div>
                                 <div>{detail.size}</div>
-                                <select></select>
-                            </div>
-                            <div>
+                                <div>
+                                    <ArrowDropDownOutlinedIcon/>
+                                </div>
+                            </main>
+                            <main>
                                 <div>Qty:</div>
                                 <div>1</div>
-                                <select></select>
-                            </div>
-                            <div>
-                                <div>{original}</div>
-                                <div>Rs.{detail.productItem.price}</div>
-                                <div>{`(${detail.productItem.discount}% OFF)`}</div>
-                            </div>
+                                <div>
+                                    <ArrowDropDownOutlinedIcon/>
+                                </div>
+                            </main>
+                        </div>
+                        <div className={styles.price}>
+                            <main>Rs.{original}</main>
+                            <div>Rs.{detail.productItem.price}</div>
+                            <div>{`(${detail.productItem.discount}% OFF)`}</div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div>
+            <div className={styles.total}>
                 <main>COUPONS</main>
-                <div>
-                    <LoyaltyOutlinedIcon />
-                    <div>Apply Coupons</div>
+                <div className={styles.apply}>
+                    <span>
+                        <LoyaltyOutlinedIcon />
+                        <div>Apply Coupons</div>
+                    </span>
                     <button>APPLY</button>
                 </div>
-                <div>Login to get upto Rs.300 OFF on the first order</div>
+                <div className={styles.flex}>
+                    <div className={styles.fontPink}>Login </div>
+                    <div>to get upto Rs.300 OFF on the first order</div>
+                </div>
                 <main>PRICE DETAILS (1 Item)</main>
-                <div>
+                <section>
                     <div>Total MRP </div>
-                    <div>Rs.{mrp}</div>
-                </div>
-                <div>
+                    <span>Rs.{mrp}</span>
+                </section>
+                <section>
                     <div>Discount on MRP</div>
-                    <div>-Rs.{totalDiscount}</div>
-                </div>
-                <div>
+                    <span>- Rs.{totalDiscount}</span>
+                </section>
+                <section>
                     <div>Coupon Discount</div>
                     <span>Apply Coupon</span>
-                </div>
-                <div>
-                    <div>
+                </section>
+                <section>
+                    <div className={styles.flex}>
                         <div>Convenience Fee</div>
-                        <div>Know More</div>
+                        <span className={styles.fontPink}>Know More</span>
                     </div>
-                    <div>Rs. 0</div>
-                </div>
-                <main>
-                    <div>Total Amount</div>
-                    <div>Rs.{total}</div>
-                </main>
+                    <span>Rs. 0</span>
+                </section>
+                <section>
+                    <main>Total Amount</main>
+                    <main>Rs.{total}</main>
+                </section>
                 <button>PLACE ORDER</button>
             </div>
         </div>
